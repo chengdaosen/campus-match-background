e85bf08474850a166f904e107d4080a52bd47885
chengdaosen <2206164816@qq.com>
2024-01-30 22:39:22 +0800

    存储用户数据到数据库

--- server/db/config.js
+++ server/db/config.js
@@ -1,9 +1,1 @@
-module.exports = {
-  mysql: {
-    host: 'localhost',
-    user: 'root',
-    password: '123456',
-    database: 'mini_program', //连接的数据库
-    port: 3306,
-  },
-}
+
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,9 +1,41 @@
-var express = require('express');
-var router = express.Router();
-
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
+const sql = require('../db/index')
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post('/', async (req, res, next) => {
+  const { code, username, headPic } = req.body
 
-module.exports = router;
+  try {
+    const wxResponse = await axios.get('https://api.weixin.qq.com/sns/jscode2session', {
+      params: {
+        appid: 'wx241b505949aaf057',
+        secret: 'cf245e57ee1fd4750c46d8181654b7c4',
+        js_code: code,
+        grant_type: 'authorization_code',
+      },
+    })
+    const openid = wxResponse.data.openid
+    // 存入数据库
+    console.log('1111', openid, username, headPic)
+    sql.query(
+      'INSERT INTO users (openId, username, head_pic) VALUES (?, ?, ?)',
+      [openid, username, headPic],
+      (error, results) => {
+        if (error) {
+          console.error('存入数据库失败:', error)
+          res.status(500).send('存入数据库失败')
+        } else {
+          console.log('数据存储成功')
+          // 返回 openid 到前端
+          res.json({ openid })
+        }
+      }
+    )
+  } catch (error) {
+    console.error('获取 openid 失败:', error)
+    res.status(500).send('获取 openid 失败')
+  }
+})
+
+module.exports = router
